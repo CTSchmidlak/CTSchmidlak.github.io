94e7f3a48dff49005c501b4e028292b335893d2a
Cooper <CTSchmidlak@gmail.com>
2025-11-10 22:45:18 -0800

    Update "dead' pages, re-direct to birthday 2025

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    plugins: [require("@tailwindcss/typography")],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+
+  plugins: [require("@tailwindcss/typography")],
+
+  theme: {
+    extend: {
+      keyframes: {
+        stripe: {
+          '0%': { backgroundPosition: '0 0' },
+          '100%': { backgroundPosition: '200% 200%' },
+        },
+      },
+      animation: {
+        stripe: 'stripe 8s linear infinite',
+      },
+    },
+  },
 };
